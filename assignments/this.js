abe503abcca16a3d9804d5512ccdb5c4429e0b98
Pere <pere.solaclaver@gmail.com>
2019-08-21 13:05:15 +0100

    Answer 4 principles and give examples for 1 and 2

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Binding - this refers to 'Window' object, meaning everything in JS. This happens when we create something on the global scope - 'this' refers to the window.
+* 2. Implicit binding - 'this' called in a method refers to the object to the left of the dot in the dot notation.
+* 3. New keyword binding - when using a constructor to instantiate objects, 'this' refers to the object being created.
+* 4. Explicit binding - 'this' is explicitely defined and refers to whatever we are passing
 *
 * write out a code example of each explanation above
 */
@@ -13,9 +13,23 @@
 
 // code example for Window Binding
 
+function speak () {
+    return this;
+}
+
 // Principle 2
 
 // code example for Implicit Binding
+
+var car = {
+    brand: 'Toyota',
+    model: 'Corolla',
+    goes: function () {
+        return `The ${this.brand} ${this.model} goes!`
+    },
+}
+
+
 
 // Principle 3
 
